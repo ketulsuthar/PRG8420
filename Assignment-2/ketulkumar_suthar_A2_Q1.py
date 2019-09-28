000a88b0930a3cbe5acc06d19fa04fd1e8030414
ketulsuthar <k2lsuthar@gmail.com>
2019-09-28 00:27:24 -0400

    Add logic for sort list and print and also add comment.

--- Assignment-2/ketulkumar_suthar_A2_Q1.py
+++ Assignment-2/ketulkumar_suthar_A2_Q1.py
@@ -1,12 +1,14 @@
 
+# This program print wors in ascending order which are input by user
 
 if __name__ == '__main__':
     word_list = []
     while True:
-        word = input('Enter some text :')
-        if word in word_list:
+        word = input('Enter some text :') # take input from user
+        if word in word_list: # this logic check word exist in list or not
             break
-        else
+        else:
             word_list.append(word)
 
-    print('/n'.join(word_list))
+    for word in sorted(word_list):# sorted function sort list into sorted order
+        print(word)
