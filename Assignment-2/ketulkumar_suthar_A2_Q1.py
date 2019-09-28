806de5d3509bfccbc243dddc0af280d3f8490c3c
ketulsuthar <k2lsuthar@gmail.com>
2019-09-28 00:41:05 -0400

    Remove if __main__ == '__main__': condition from logic. We need to import this file for testing so may be it will not working .

--- Assignment-2/ketulkumar_suthar_A2_Q1.py
+++ Assignment-2/ketulkumar_suthar_A2_Q1.py
@@ -1,17 +1,16 @@
 
 # This program print words in ascending order which are input by user
 
-if __name__ == '__main__':
-    word_list = []
-    while True:
-        try:
-            word = input('Enter some text :') # take input from user
-            if word in word_list: # this logic check word exist in list or not
-                break
-            else:
-                word_list.append(word)
-        except Exception as e:
-            print(e)
+word_list = []
+while True:
+    try:
+        word = input('Enter some text :') # take input from user
+        if word in word_list: # this logic check word exist in list or not
+            break
+        else:
+            word_list.append(word)
+    except Exception as e:
+        print(e)
 
-    for word in sorted(word_list):# sorted function sort list into sorted order
-        print(word)
+for word in sorted(word_list):# sorted function sort list into sorted order
+    print(word)
