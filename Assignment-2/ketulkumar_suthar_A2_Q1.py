32c59173211e02983496ac818566cbaf66ea729c
ketulsuthar <k2lsuthar@gmail.com>
2019-09-28 00:30:45 -0400

    Add exception handling code.

--- Assignment-2/ketulkumar_suthar_A2_Q1.py
+++ Assignment-2/ketulkumar_suthar_A2_Q1.py
@@ -4,11 +4,14 @@
 if __name__ == '__main__':
     word_list = []
     while True:
-        word = input('Enter some text :') # take input from user
-        if word in word_list: # this logic check word exist in list or not
-            break
-        else:
-            word_list.append(word)
+        try:
+            word = input('Enter some text :') # take input from user
+            if word in word_list: # this logic check word exist in list or not
+                break
+            else:
+                word_list.append(word)
+        except Exception as e:
+            print(e)
 
     for word in sorted(word_list):# sorted function sort list into sorted order
         print(word)
